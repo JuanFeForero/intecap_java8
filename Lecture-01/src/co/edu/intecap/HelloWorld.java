a5bb955a6d294ada2dcce297a8328599688dccd3
cesar.hoyos <cesar07hoyos03@gmail.com>
2017-02-03 15:52:10 -0500

    Lecture control

---
+++ Lecture-01/src/co/edu/intecap/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.intecap;
+
+/**
+ *
+ * @author cesarhoyos
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Java8 course!");
+    }
+}
